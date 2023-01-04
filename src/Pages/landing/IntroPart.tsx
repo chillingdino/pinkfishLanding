665dc8c52f40016639a09dbdf7f9ce7970f19cb1
klopkloppp12 <84202194+bcvent@users.noreply.github.com>
2023-01-04 23:21:00 +0100

    added memberships

--- src/Pages/landing/IntroPart.tsx
+++ src/Pages/landing/IntroPart.tsx
@@ -1,6 +1,7 @@
 import { Container } from "components";
 import Button from "components/Button";
 import { Header } from "layout";
+import React from "react";
 import { useCallback, useEffect, useState } from "react";
 import { FaArrowDown } from "react-icons/fa";
 import {
@@ -8,12 +9,17 @@ import {
   IntroButtonWrapper,
   IntroCardBody,
   IntroCardGroup,
+  IntroCardGroupMemership,
+  //IntroCardMembershipBody,
   IntroCardWrapper,
   IntroTitleWrapper,
   IntroWrapper,
+  LeftCardBody,
+  LeftGroupBody,
+  RightGroupBody,
   WelcomeWrapper,
 } from "./landing.styled";
- 
+//import homeFrame from "./HomeMFrame.png"
 const cardData = [
   {
     title: "Treasury-Backed Insurance",
@@ -32,6 +38,27 @@ const cardData = [
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum, quam eu fermentum fringilla",
   },
 ];
+const membershipData = [
+  /*{
+    type: "d1",
+    //img:homeFrame
+  },*/
+  {
+    title: "Treasury-Backed Insurance",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    type: "a1",
+  },
+  {
+    title: "Passive Ethereum Reflections",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  ",
+    type: "a2",
+  },
+  {
+    title: "Optimized Treasury Investment",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  ",
+    type: "a3",
+  },
+];
 
 const IntroCard = (props: any) => {
   return (
@@ -41,19 +68,51 @@ const IntroCard = (props: any) => {
     </IntroCardBody>
   );
 };
+/*
+const IntroCardMemberships = (props: any) => {
+  return (
+    <IntroCardMembershipBody type={props.type}>
+      {props.type != "d1" ? (
+        <React.Fragment>
+          <img></img>
+          <div>
+            <h2>{props.title}</h2>
+            <p>{props.desc}</p>
+          </div>
+        </React.Fragment>
+      ) : (
+        <img src={"./HomeMFrame.png"} />
+      )}
+    </IntroCardMembershipBody>
+  );
+};
+*/
+
+const LeftCardBodyy = (props: any) => {
+  return (
+    <LeftCardBody type={props.type}>
+      <React.Fragment>
+        <img></img>
+        <div>
+          <h2>{props.title}</h2>
+          <p>{props.desc}</p>
+        </div>
+      </React.Fragment>
+    </LeftCardBody>
+  );
+};
 
 const IntroPart = () => {
   const [y, setY] = useState(0);
-  
+
   useEffect(() => {
     const titleSection: any = document.getElementById("intro-title");
     const icon: any = document.getElementById("arrowDiv");
     titleSection.style.opacity = "1";
     titleSection.style.transform = "scale(1)";
 
-     icon.style.opacity="1";
-     icon.style.transform = "scale(1)";
-
+    icon.style.opacity = "1";
+    icon.style.transform = "scale(1)";
   }, []);
 
   const handleScroll = useCallback(
@@ -62,15 +121,15 @@ const IntroPart = () => {
       const activeDiv: any = document.getElementById("intro-why");
       const arrowDiv: any = document.getElementById("arrowDiv");
 
-      console.log("s: "+ currentTarget.scrollY);
-      console.log("y: "+ y);
+      console.log("s: " + currentTarget.scrollY);
+      console.log("y: " + y);
       if (y > currentTarget.scrollY) {
         if (activeDiv.getBoundingClientRect().top > window.innerHeight - 50) {
           activeDiv.style.transform = "translateY(100px)";
           activeDiv.style.opacity = "0";
           console.log("giu");
         }
-      } else   {
+      } else {
         if (activeDiv.getBoundingClientRect().top <= window.innerHeight - 50) {
           activeDiv.style.transform = "translateY(0)";
           activeDiv.style.opacity = "1";
@@ -89,7 +148,7 @@ const IntroPart = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [handleScroll]);
-//console.log(y);
+  //console.log(y);
   return (
     <IntroWrapper>
       <WelcomeWrapper>
@@ -102,14 +161,18 @@ const IntroPart = () => {
             <h2>Not only a Web3 content creator platform</h2>
             <p />
             <IntroButtonWrapper>
-              <Button label="Web App" type="white" disabled="true" onClick={() => {}} />
+              <Button
+                label="Web App"
+                type="white"
+                disabled="true"
+                onClick={() => {}}
+              />
             </IntroButtonWrapper>
           </IntroTitleWrapper>
         </Container>
-        
       </WelcomeWrapper>
       <ArrowDiv id="arrowDiv">
-        <FaArrowDown className="arrow" id="icon"/> 
+        <FaArrowDown className="arrow" id="icon" />
       </ArrowDiv>
 
       <Container>
@@ -121,6 +184,38 @@ const IntroPart = () => {
               <IntroCard key={key} title={item.title} desc={item.desc} />
             ))}
           </IntroCardGroup>
+
+          <IntroCardGroupMemership>
+            <LeftGroupBody>
+            <h2>
+              Memerships
+            </h2>
+              {membershipData.map((item: any, key: any) => (
+                <LeftCardBodyy
+                  key={key}
+                  title={item.title}
+                  desc={item.desc}
+                  img={item.img}
+                />
+              ))}
+
+            </LeftGroupBody>
+            <RightGroupBody>
+              <img src={"./HomeMFrame.png"} />
+            </RightGroupBody>
+          </IntroCardGroupMemership>
+          {/*<IntroCardGroupMemership>
+            {membershipData.map((item: any, key: any) => (
+              <IntroCardMemberships
+                key={key}
+                title={item.title}
+                desc={item.desc}
+                type={item.type ? item.type : "d1"}
+                img={item.img}
+              />
+            ))}
+          </IntroCardGroupMemership>
+          */}
         </IntroCardWrapper>
       </Container>
     </IntroWrapper>
--- src/Pages/landing/landing.styled.tsx
+++ src/Pages/landing/landing.styled.tsx
@@ -3,15 +3,19 @@ import styled from "styled-components";
 import introBG from "assets/image/bg.svg";
 
 export const LandingWrapper = styled.div`
-  background: linear-gradient(270deg, #FFFDD3 0%, #BDC6EC 50%, #FFFDD3 100%);
+  background: linear-gradient(270deg, #fffdd3 0%, #bdc6ec 50%, #fffdd3 100%);
 
   @media screen and (max-width: 768px) {
-    background: linear-gradient(270deg, #FFFDD3 0%, #CED5F2 25%, #CED5F2 75%, #FFFDD3 100%);
+    background: linear-gradient(
+      270deg,
+      #fffdd3 0%,
+      #ced5f2 25%,
+      #ced5f2 75%,
+      #fffdd3 100%
+    );
   }
   min-height: -webkit-fill-available;
 `;
-
-
 
 export const IntroWrapper = styled.div`
   /* background: url(${introBG}); */
@@ -29,20 +33,18 @@ export const ArrowDiv = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  .arrow{
+  .arrow {
     color: white;
-    font-size: 56px; 
-    
+    font-size: 56px;
   }
   transition: all 1s;
   opacity: 0;
   transform: scale(0);
- 
 `;
 
 export const WelcomeWrapper = styled.div`
   /*min-height: calc(100vh - 64px);*/
-  height:calc(var(--vh, 1vh) * 100 - 64px);
+  height: calc(var(--vh, 1vh) * 100 - 64px);
 `;
 export const IntroTitleWrapper = styled.div`
   transition: all 1s;
@@ -59,20 +61,19 @@ export const IntroTitleWrapper = styled.div`
       line-height: 50px;
     }
     margin-bottom: 4px;
-    
   }
   span {
-      vertical-align: middle;
-      font-size: 16px;
-      background: rgba(0, 0, 0, 0.06);
-      border-radius: 12px;
-      padding: 6px 10px;
-      margin-left: 12px;
-      /* text-transform: uppercase; */
-      font-weight: 700;
-      /* letter-spacing: 2px; */
-      color: rgba(0, 0, 0, 0.48);
-    }
+    vertical-align: middle;
+    font-size: 16px;
+    background: rgba(0, 0, 0, 0.06);
+    border-radius: 12px;
+    padding: 6px 10px;
+    margin-left: 12px;
+    /* text-transform: uppercase; */
+    font-weight: 700;
+    /* letter-spacing: 2px; */
+    color: rgba(0, 0, 0, 0.48);
+  }
   h2 {
     font-size: 24px;
     font-weight: 600;
@@ -93,7 +94,7 @@ export const IntroCardWrapper = styled.div`
   transition: all 1s;
   text-align: center;
   transform: translateY(100px);
-  opacity:0;
+  opacity: 0;
   h1,
   h2,
   p {
@@ -125,7 +126,8 @@ export const IntroCardGroup = styled.div`
   grid-gap: 32px;
   margin-top: 24px;
   @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(1, minmax(0, 384px));
+    justify-content: center;
   }
 `;
 
@@ -142,107 +144,147 @@ export const IntroCardBody = styled.div`
     line-height: 28px;
   }
   p {
+    font-size: 16px;
+    line-height: 22px;
+    color: #131419;
+  }
+  max-width: 384px;
+`;
+/*
+style 3 blocks white on left 
+export const IntroCardGroupMemership = styled.div`
+  gap: 32px 32px;
+  margin-top: 24px;
+  @media screen and (max-width: 768px) {
+    /*grid-template-columns: repeat(1, minmax(0, 384px));
+    grid-template-rows: repeat(4, minmax(1, 384px));
+    justify-content: center;
+    grid-template-areas:
+    "d1"
+    "a1"
+    "a2"
+    "a3"; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+
+  }
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-areas:
+    "a1 d1"
+    "a2 d1"
+    "a3 d1";
+`;
+
+export const IntroCardMembershipBody = styled.div<{ type: string }>`
+  grid-area: ${(props) => (props.type ? props.type : "d1")};
+  text-align: left;
+  padding: 0px;
+  margin-top: 32px;
+  display: flex;
+  align-items: flex-start;
+  max-width:384px;
+
+  border-radius: 20px;
+  box-shadow: rgb(42 73 108 / 20%) 0px 8px 40px;
+  background-color: #fff;
+
+  img {
+    min-width: ${(props) => (props.type === "d1" ? "" : "48px")};
+    min-height: ${(props) => (props.type === "d1" ? "" : "48px")};
+    margin-right: ${(props) => (props.type === "d1" ? "" : "16px")};
+    width: 100%;
+    //max-width: 100%;
+    max-width: ${(props) => (props.type === "d1" ? "100%" : "48px")};
+  }
+  h2 {
+    font-size: 14px;
+    line-height: 22px;
+    color: #131419;
+  }
+  p {
     font-size: 14px;
     line-height: 22px;
     color: #131419;
   }
 `;
+*/
 
-export const StableWrapper = styled.div`
-  padding: 90px 0;
+export const IntroCardGroupMemership = styled.div`
+  gap: 32px 32px;
+  margin-top: 24px;
+  @media screen and (max-width: 768px) {
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+  }
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-areas: "a1 d1";
 `;
 
-export const StableTitleWrapper = styled.div`
-  h1,
-  p {
-    margin: 0;
-    padding: 0;
-  }
-  h1 {
-    font-size: 36px;
-    text-align: center;
-    line-height: 35px;
-    color: black !important;
-  }
-  & > p {
-    font-weight: 500;
-    font-size: 18px;
-    text-align: center;
-    z-index: 9999;
-    line-height: 20px;
-    margin-top: 10px;
-    background-color: #f3ec78;
-    background-image: linear-gradient(120deg, #4986e7 0%, #6ac9f5 100%);
-    background-size: 100%;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-`;
-
-export const StableCardWrapper = styled.div`
-  padding: 50px;
-  margin: 50px auto 0px;
-  max-width: 820px;
+export const LeftGroupBody = styled.div`
+  grid-area: a1;
   border-radius: 20px;
-  background-color: rgb(195, 198, 212);
-  box-shadow: rgb(0 0 0 / 30%) 0px 8px 32px;
-  position: relative;
-  box-sizing: border-box;
-  max-width: 638px;
-  width: 100%;
-`;
+  box-shadow: rgb(42 73 108 / 20%) 0px 8px 40px;
+  background-color: #fff;
 
-export const StableButtonGroup = styled.div`
+  height: max-content;
+  padding: 16px;
   display: flex;
-  justify-content: space-around;
-  margin-bottom: 40px;
-`;
-
-export const StableForm = styled.div`
-  width: 90%;
-  margin: auto;
-`;
-
-export const StableFormLabel = styled.div`
-  display: flex;
-  justify-content: space-between;
-  p {
+  flex-direction: column;
+  justify-content: start;
+  h2 {
+    margin-bottom: 8px;
     font-weight: 600;
+    font-size: 18px;
+    line-height: 28px;
   }
 `;
+export const RightGroupBody = styled.div`
+  grid-area: d1;
+  border-radius: 20px;
+  box-shadow: rgb(42 73 108 / 20%) 0px 8px 40px;
+  background-color: #fff;
 
-export const StableInput = styled.div`
-  margin-top: 10px;
-  display: flex;
-  border-radius: 100px;
-  background: white;
-  padding: 10px 20px;
-  input {
-    border: none;
-    outline: none;
-    width: -webkit-fill-available;
+  max-width: 384px;
+
+  img {
+    width: 100%;
+    max-width: 100%;
   }
+
+
 `;
 
-export const ExchangeSymbol = styled.div`
-  cursor: pointer;
+export const LeftCardBody = styled.div<{ type: string }>`
+  text-align: left;
+  margin-top: 16px;
+  margin-bottom: 16px;
   display: flex;
-  margin: 40px auto;
-  align-items: center;
-  justify-content: center;
-  width: 40px;
-  height: 40px;
-  background-color: rgb(150, 159, 201);
-  border-radius: 100px;
-  color: white;
-  svg {
-    transform: rotate(90deg);
-  }
-`;
+  align-items: flex-start;
+  max-width: 384px;
 
-export const StableSwapBtn = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 30px;
+  img {
+    min-width:   48px  ;
+    min-height:  48px ;
+    margin-right: 16px;
+    width: 100%;
+    max-width: 48px;
+  }
+  h2 {
+    font-size: 16px;
+    line-height: 22px;
+    color: #131419;
+  }
+  p {
+    font-size: 16px;
+    line-height: 22px;
+    color: #131419;
+  }
 `;
